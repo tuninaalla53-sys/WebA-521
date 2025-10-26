99632d0eaf7adb2d8f4086ba9ccad9ac1a4607cf
Alla <tuninaalla53@gmail.com>
2025-10-26 17:25:16 +0300

    Revert "Добавлены сущности Фильмы и Режиссеры"
    This reverts commit 826e7516752f6a4fcc7c7690e4feaffe97030205.

--- GHJE/COMPOSE/resources/views/city/list.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Список городов</h1>
-
-<ul>
-    <li><a href="{{ route("city.index", ["id" => 1]) }}">Вологда</a></li>
-    <li><a href="{{ route("city.index", ["id" => 2]) }}">Кингисепп</a></li>
-</ul>
